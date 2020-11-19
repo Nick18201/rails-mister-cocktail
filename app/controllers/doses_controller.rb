ec026881a5963dbbd0c913bc8e09f08fd732bfc0
nicolas blum <nblumferracci@gmail.com>
2020-11-19 16:14:06 +0100

    all features done for dose and cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,8 +1,34 @@
 class DosesController < ApplicationController
-def new
-end
+  def new
+    set_cocktail
+    @dose = Dose.new
+  end
 
-def create
-end
+  def create
+    set_cocktail
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
 
+  def destroy
+
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
 end
