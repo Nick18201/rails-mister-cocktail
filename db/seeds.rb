9049a0262e254d69906718d1094d08b431074b70
nicolas blum <nblumferracci@gmail.com>
2020-11-19 12:54:47 +0100

    json parsed for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "destroy DB"
+Ingredient.destroy_all
+
+puts "parsing json and generating seed for ingredients"
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+if Ingredient.count == 0
+  user_serialized = open(url).read
+  user = JSON.parse(user_serialized)
+  user["drinks"].each do |ingredient|
+    Ingredient.create!(name: ingredient["strIngredient1"])
+  end
+  puts "ingredients are seeded"
+end
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "rhum")
+# Ingredient.create(name: "sugar syrup")
+# Ingredient.create(name: "vodka")
+# Ingredient.create(name: "grenadine")
+# Ingredient.create(name: "tequila")
+# Ingredient.create(name: "orange Juice")
+# Ingredient.create(name: "pepper")
+# Ingredient.create(name: "tomato juice")
+# Ingredient.create(name: "basilic")
+# Ingredient.create(name: "tonic")
+# Ingredient.create(name: "redbull")
+# Ingredient.create(name: "whisky")
+# Ingredient.create(name: "amaretto")
